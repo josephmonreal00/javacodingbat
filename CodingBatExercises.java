5a381af6fe1f46a2832ed3d6322a77c3a8b4abd2
Joseph Monreal <joemonreal23@icloud.com>
2019-10-01 01:36:38 -0700

    Practice: 09-30-2019

--- CodingBatExercises.java
+++ CodingBatExercises.java
@@ -837,6 +837,244 @@ public class CodingBatExercises {
   		return str.substring(2, str.length()) + str.substring(0,2);
 	}
 
+	/*
+	Given a string, return a string length 1 from its front, unless front is false, in which 
+	case return a string length 1 from its back. The string will be non-empty.
+
+	theEnd("Hello", true) → "H"
+	theEnd("Hello", false) → "o"
+	theEnd("oh", true) → "o"
+	*/
+
+	public String theEnd(String str, boolean front) {
+  		if(front == true) {
+    		return str.substring(0,1);
+  		}
+  		return str.substring(str.length() - 1, str.length());
+	}
+
+	/*
+	Given a string, return a version without both the first 
+	and last char of the string. The string may be any length, including 0.
+
+	withouEnd2("Hello") → "ell"
+	withouEnd2("abc") → "b"
+	withouEnd2("ab") → ""
+	*/
+
+	public String withouEnd2(String str) {
+	  	if(str.length() == 0  || str.length() == 1) {
+	    	return "";
+	  	}
+	  	return str.substring(1,str.length() - 1);
+	}
+
+	/*
+	Given a string of even length, return a string made of the middle two chars, 
+	o the string "string" yields "ri". The string length will be at least 2.
+
+	middleTwo("string") → "ri"
+	middleTwo("code") → "od"
+	middleTwo("Practice") → "ct"
+	*/
+
+	public String middleTwo(String str) {
+  		int middle = str.length() / 2;
+  		return str.substring(middle - 1, middle + 1);
+	}
+
+	/*
+	Given a string, return true if it ends in "ly".
+
+	endsLy("oddly") → true
+	endsLy("y") → false
+	endsLy("oddy") → false
+	*/
+	public boolean endsLy(String str) {
+	  	if(str.length() <= 1) {
+	    	return false;
+	  	}
+	  	if(str.substring(str.length() - 2, str.length()).equals("ly")) {
+	    	return true;
+	  	}
+	  	return false;
+	}
+
+	/*
+	Given a string and an int n, return a string made of the first and last n chars 
+	from the string. The string length will be at least n.
+
+	nTwice("Hello", 2) → "Helo"
+	nTwice("Chocolate", 3) → "Choate"
+	nTwice("Chocolate", 1) → "Ce"
+	*/
+	public String nTwice(String str, int n) {
+  		return str.substring(0, n) + str.substring(str.length() - n, str.length());
+	}
+
+	/*
+	Given a string and an index, return a string length 2 starting at the given index. If the index is too big or too small to define a string 
+	length 2, use the first 2 chars. The string length will be at least 2.
+
+	twoChar("java", 0) → "ja"
+	twoChar("java", 2) → "va"
+	twoChar("java", 3) → "ja"
+	*/
+
+	public String twoChar(String str, int index) {
+  		if(str.length() - 1 == index || index == str.length() || index < 0 || index > str.length()) {
+    		return str.substring(0, 2);
+  		}
+  		return str.substring(index, index + 2);
+	}
+
+	/*
+	Given a string of odd length, return the string length 3 from its middle, so 
+	"Candy" yields "and". The string length will be at least 3.
+
+	middleThree("Candy") → "and"
+	middleThree("and") → "and"
+	middleThree("solving") → "lvi"
+	*/
+
+	public String middleThree(String str) {
+  		if(str.length() < 3) {
+    		return str;
+  		}
+		int middle = str.length() / 2;
+		return str.substring(middle - 1, middle + 2);
+	}
+
+	/*
+	Given a string, return true if "bad" appears starting at index 0 or 1 in the string, such as with "badxxx" or "xbadxx" but not "xxbadxx". The string may be any length, including 0. Note: use .equals() to compare 2 strings.
+
+	hasBad("badxx") → true
+	hasBad("xbadxx") → true
+	hasBad("xxbadxx") → false
+	*/
+
+	public boolean hasBad(String str) {
+  		if(str.length() < 3 || str.length() == 3 && !(str.substring(0,3).equals("bad"))) {
+    		return false;
+  		}
+  		if(str.substring(0,3).equals("bad") || str.substring(1,4).equals("bad")) {
+    		return true;
+  		}
+  		return false;
+	}
+
+	/*
+	Given a string, return a string length 2 made of its first 2 chars. If the string length is less than 2, use '@' for the missing chars.
+
+	atFirst("hello") → "he"
+	atFirst("hi") → "hi"
+	atFirst("h") → "h@"
+	*/
+	public String atFirst(String str) {
+  		if(str.length() == 0) {
+    		return "@@";
+  		}
+  		if(str.length() == 1) {
+    		return str.substring(0,1) + "@";
+  		}
+  		return str.substring(0,2);
+	}
+
+	/*
+	Given 2 strings, a and b, return a new string made of the first char of a and 
+	the last char of b, so "yo" and "java" yields "ya". If either string is length 0, use '@' for its missing char.
+
+	lastChars("last", "chars") → "ls"
+	lastChars("yo", "java") → "ya"
+	lastChars("hi", "") → "h@"
+	*/
+
+	public String lastChars(String a, String b) {
+	  	if(a.length() == 0 && b.length() == 0) {
+	    	return "@@";
+	  	}
+	  	if(a.length() > 0 && b.length() == 0) {
+	    	return a.substring(0,1) + "@";
+	  	}
+	  	if(a.length() == 0 && b.length() > 0) {
+	    	return "@" + b.substring(b.length() - 1, b.length());
+	  	}
+	  	return a.substring(0,1) + b.substring(b.length() - 1, b.length());
+	}
+
+	/*
+	Given two strings, append them together (known as "concatenation") and return the result. However, if the concatenation creates a double-char, then omit one of the chars, so "abc" and "cat" yields "abcat".
+
+	conCat("abc", "cat") → "abcat"
+	conCat("dog", "cat") → "dogcat"
+	conCat("abc", "") → "abc"
+	*/
+	public String conCat(String a, String b) {
+  		if(a.length() == 0) {
+    		return b;
+  		}
+  		if(b.length() == 0) {
+    		return a;
+  		}
+  		if(a.substring(a.length() - 1, a.length()).equals(b.substring(0,1))) {
+    		return a.substring(0, a.length() - 1) + b;
+  		}
+  		return a + b;
+	}
+
+	/*
+	Given a string of any length, return a new string where the last 2 chars, 
+	if present, are swapped, so "coding" yields "codign".
+
+	lastTwo("coding") → "codign"
+	lastTwo("cat") → "cta"
+	lastTwo("ab") → "ba"
+	*/
+	public String lastTwo(String str) {
+	  	if(str.length() <= 1) {
+	    	return str;
+	  	}
+	  	String secondToLast = str.substring(str.length() - 2, str.length() - 1);
+	  	String last = str.substring(str.length() - 1, str.length());
+	  	return str.substring(0,str.length() - 2) + last + secondToLast;
+	}
+
+	/*
+	Given a string, if the string begins with "red" or "blue" return 
+	that color string, otherwise return the empty string.
+
+	seeColor("redxx") → "red"
+	seeColor("xxred") → ""
+	seeColor("blueTimes") → "blue"
+	*/
+
+	public String seeColor(String str) {
+	  	if(str.length() <= 2) {
+	    	return "";
+	  	}
+	  	if(str.substring(0,3).equals("red")) {
+	    	return "red";
+	  	}
+	  	if(str.length() == 3 && !(str.substring(0,2).equals("red"))) {
+	    	return "";
+	  	}
+	  	if(str.substring(0,4).equals("blue")) {
+	    	return "blue";
+	  	}
+	  	return "";
+	}
+
+
+
+
+
+
+
+
+
+
+
+
 
 	/*
 	█▀▀█ █▀▀█ █▀▀█ █▀▀█ █░░█   ▄█░
