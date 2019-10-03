eb676b836fc22d4c3ed171d9c2e814feb9df3f6d
Joseph Monreal <joemonreal23@icloud.com>
2019-10-02 21:29:43 -0700

    Practice 10-02-2019

--- CodingBatExercises.java
+++ CodingBatExercises.java
@@ -1065,6 +1065,87 @@ public class CodingBatExercises {
 	}
 
 
+	/*
+	Given two strings, append them together (known as "concatenation") and return the result. However, if the strings are different lengths, omit chars from the longer string so it is the same length as the shorter string. So "Hello" and "Hi" yield "loHi". The strings may be any length.
+
+	minCat("Hello", "Hi") → "loHi"
+	minCat("Hello", "java") → "ellojava"
+	minCat("java", "Hello") → "javaello"
+	*/
+	public String minCat(String a, String b) {
+  		int length = b.length();
+  		if(b.length() > a.length()) {
+    		int a_length = a.length();
+    		return a + b.substring(b.length() - a_length, b.length());
+  		}
+  		return a.substring(a.length() - length, a.length()) + b;
+	}
+
+	/*
+	Given a string, return a new string made of 3 copies of the first 2 chars of the original string. The string may be any length. If there are fewer than 2 chars, use whatever is there.
+
+	extraFront("Hello") → "HeHeHe"
+	extraFront("ab") → "ababab"
+	extraFront("H") → "HHH"
+	*/
+
+	public String extraFront(String str) {
+  		if(str.length() == 0) {
+    		return "";
+  		}
+  		if(str.length() == 1) {
+    		return str + str + str;
+  		}
+  		return str.substring(0,2) + str.substring(0,2) + str.substring(0,2);
+	}
+
+
+	/*
+	Given a string, if a length 2 substring appears at both its beginning and end, return a string without the substring at the beginning, so "HelloHe" yields "lloHe". The substring may overlap with itself, so "Hi" yields "". Otherwise, return the original string unchanged.
+
+	without2("HelloHe") → "lloHe"
+	without2("HelloHi") → "HelloHi"
+	without2("Hi") → ""
+	*/
+	public String without2(String str) {
+  		if(str.length() == 0) {
+    		return "";
+  		}
+  		if(str.length() == 1) {
+    		return str;
+  		}
+  		if(str.substring(0,2).equals(str.substring(str.length() - 2, str.length()))) {
+    		return str.substring(2,str.length());
+  		}
+  		return str;
+	}
+
+
+	/*
+	Given a string, return a version without the first 2 chars. Except keep the first char if it is 'a' and keep the second char if it is 'b'. The string may be any length. Harder than it looks.
+
+	deFront("Hello") → "llo"
+	deFront("java") → "va"
+	deFront("away") → "aay"
+	*/
+	public String deFront(String str) {    
+  		if(str.substring(0,1).equals("a") && !(str.substring(1,2).equals("b"))) {
+    		return str.substring(0,1) + str.substring(2,str.length());
+  		}
+  		if(!(str.substring(0,1).equals("a")) && (str.substring(1,2).equals("b"))) {
+    		return str.substring(1,str.length());
+  		}
+ 		 if(!(str.substring(0,1).equals("a")) && !(str.substring(1,2).equals("b"))) {
+    		return str.substring(2, str.length());
+  		}
+  		return str;
+	}
+
+
+
+
+
+
 
 
 
